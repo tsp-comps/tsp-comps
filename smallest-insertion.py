f1db231fb423c7963261f3b652881c23f68c5913
Artem Yushko <yushko2002@gmail.com>
2025-02-03 11:23:14 -0600

    made it up to date

---
+++ smallest-insertion.py
@@ -0,0 +1,18 @@
+import tsplib95
+import networkx as nx
+
+def process_tsp95(file_path):
+    tsp_set = tsplib95.load(file_path)
+    g_tsp = tsp_set.get_graph()
+    return g_tsp
+
+def main():
+
+    number = input("Your sentence: ") # user input
+    if len(errorful_sentence) == 0: # if tsp95, then go there
+        
+
+    return
+
+if __name__=="__main__":
+    main()
