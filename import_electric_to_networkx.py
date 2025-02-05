c8eebe582f6d0dd567d2b241b92359cba5979edd
Arthur Viegas Eguia <viegasarthureguia@gmail.com>
2025-02-05 11:24:19 -0600

    adding the code to read the electric grid dataset to code

--- import_electric_to_networkx.py
+++ import_electric_to_networkx.py
@@ -1,14 +1,16 @@
 import networkx
 
-#Here we read the cleaned data from a file, make their format friendly to networkx
-#and add them to an array. These are the edges of our graph
-file = open("datasets/electric-grid/electric_points.tsv", "r")
-edges = []
-for line in file:
-    split_line = line.split("\t")
-    edges.append(tuple(split_line))
-file.close()
+def load_electric_grid_dataset():
+    #Here we read the cleaned data from a file, make their format friendly to networkx
+    #and add them to an array. These are the edges of our graph
+    file = open("datasets/electric-grid/electric_points.tsv", "r")
+    edges = []
+    for line in file:
+        split_line = line.split("\t")
+        edges.append(tuple(split_line))
+    file.close()
 
-#Here we create the graph and load the edges
-graph = networkx.Graph()
-graph.add_weighted_edges_from(edges)
+    #Here we create the graph and load the edges
+    graph = networkx.Graph()
+    graph.add_weighted_edges_from(edges)
+    return graph
