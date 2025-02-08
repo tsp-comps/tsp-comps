be402405e0f85643845d98dd1408bd98f401e22e
Artem Yushko <yushko2002@gmail.com>
2025-02-07 19:18:29 -0600

    wrote the smallest insertion algorithm

--- smallest-insertion.py
+++ smallest-insertion.py
@@ -1,3 +1,4 @@
+import random
 import networkx as nx
 
 class SmallestInsertion(object):
@@ -8,15 +9,41 @@ class SmallestInsertion(object):
         """
         Solve the smallest insertion problem.
         """
-        # create a new graph to store the solution
-        solution = nx.Graph()
-        solution.add_node(0)
+        # randomly select two nodes and set them as a graph g
+        nodes = list(graph.nodes)
+        tour = random.sample(nodes, 2)
+        unvisited = [node for node in nodes if node not in tour]
+        currweight = graph[tour[0]][tour[1]]['weight']
 
-        # add the remaining nodes to the solution
-        while len(solution.nodes) < len(graph.nodes):
-            node = self.find_closest_node(graph, solution)
-            solution.add_node(node)
-            path = self.find_shortest_path(solution, node)
-            solution = self.insert_node(solution, path, node)
+        while unvisited:
+            best_node = None
+            best_cost_increase = float('inf')
+            best_position = None
 
-        return solution
+            # look at the unvisited node v
+            for u in unvisited:
+
+                for i in range(len(tour)):
+                    # find the tentative cumulative edge weight of g by adding v to nearest node in g                    
+                    if graph[u][tour[i]]['weight'] < best_cost_increase:
+                        best_cost_increase = graph[u][tour[i]]['weight']
+                        # update the best solution
+                        best_node = u
+                        best_position = i + 1  
+
+                # insert the best node into the best position
+                currweight += best_cost_increase
+                tour.insert(best_position, best_node)
+                unvisited[:] = [node for node in unvisited if node != best_node]
+
+        return currweight
+    
+# for testing purposes
+from datasets import Datasets
+
+if __name__ == "__main__":
+    si = SmallestInsertion()
+    graph = Datasets.process_tsp95("datasets/tsp95/wi29.tsp")
+    print(si.solve(graph))
+    sol = nx.approximation.traveling_salesman_problem(graph)
+    print(sum([graph[sol[i]][sol[i+1]]['weight'] for i in range(len(sol)-1)]))
