65cae5689f805dbef66ebb009ced29e871d6c3b9
DanielChen29 <daniel.yp.chen@gmail.com>
2025-02-10 16:33:39 -0600

    Began combining christofides pieces

---
+++ christofides.py
@@ -0,0 +1,146 @@
+import networkx as nx
+
+# helper function to find the root of a given node in a dictionary
+def find(parent, n):
+    if parent[n] == n:
+        return n
+    return find(parent, parent[n])
+
+# helper function to union two sets
+def union(parent, x, y):
+    x_set = find(parent, x)
+    y_set = find(parent, y)
+    if x_set != y_set:
+        parent[x_set] = y_set
+
+'''takes a networkx graph and returns a minimum spanning tree of that graph using Kruskal's algorithm'''
+def kruskals_algorithm(graph):
+        mst = nx.Graph()    
+        
+        edges = sorted(list(graph.edges(data=True)), key=lambda x: x[2]['weight'])
+
+        # i is the index of the edge we are considering and e is the number of edges we have added to the mst
+        i = 0
+        e = 0
+
+        # parent is a dictionary that keeps track of the parent of each node
+        parent = {node: node for node in graph.nodes()}
+
+        # while we haven't added all edges to our mst, we consider the next edge and if it doesn't
+        # create a cycle, we add it to the mst.
+        while e < graph.number_of_nodes() - 1:
+            u, v, w = edges[i]
+            i += 1
+
+            # checks for cycle creation. If the roots of u and v are the same, then there is a cycle
+            x = find(parent, u)
+            y = find(parent, v)
+            if x != y:
+                e += 1
+                mst.add_edge(u, v, weight=w['weight'])
+                union(parent, x, y)
+
+        return mst
+
+def is_odd(number):
+     if number % 2 == 1:
+          return True
+     return False
+
+'''takes a networkx graph and returns a subgraph of the nodes with odd degree (part two of christofides algorithm)'''
+def find_odd_degree(graph):
+    odd_degree = nx.Graph()
+    for node in graph.nodes():
+        if is_odd(graph.degree(node)):
+            odd_degree.add_node(node)
+    for u, v, w in graph.edges(data=True):
+        if u in odd_degree.nodes() and v in odd_degree.nodes():
+            odd_degree.add_edge(u, v, weight=w['weight'])
+    return odd_degree
+
+''' takes a graph and returns a minimum weight perfect matching of the graph'''
+def blossom_algorithm(graph):
+    return nx.algorithms.matching.min_weight_matching(graph, weight="weight")
+
+def dfs_counter(graph, current, visited_nodes):
+    neighbors = list(graph.neighbors(current))
+    count = 1
+    visited_nodes[current] = True
+    for node in neighbors:
+        if visited_nodes[node] == False:
+            count += dfs_counter(graph, node, visited_nodes)
+    return count
+
+def is_valid_edge(graph, start, end):
+    nodes = list(graph.nodes)
+    start_degree = graph.degree(start)
+    #valid if degree is 1
+    if start_degree == 1:
+        return True
+     
+    #valid if the edge is not a bridge
+    visited_nodes = {}
+    for node in nodes:
+        visited_nodes.update({node : False})
+    count1 = dfs_counter(graph, start, visited_nodes)
+
+    for node in nodes:
+        visited_nodes.update({node : False})
+    print(graph.edges)
+    print(start)
+    print(end)
+    graph.remove_edge(start,end)
+    count2 = dfs_counter(graph, start, visited_nodes)
+     
+    graph.add_edge(start,end)
+
+    if count1 > count2:
+        return False
+    return True
+     
+        
+'''Finds the Eulerian tour for the given graph, beginning at the initial input current node'''
+def get_eulerian_tour(graph, current, path):
+    neighbors = list(graph.neighbors(current))
+    for neighbor in neighbors:
+        print(path)
+        if is_valid_edge(graph, current, neighbor):
+            path.append(neighbor)
+            graph.remove_edge(current,neighbor)
+            return get_eulerian_tour(graph,neighbor,path)
+    return path
+    
+'''Takes an Eulerian graph and returns an Eulerian tour of the graph.'''
+def fleurys_algorithm(graph):
+    nodes = list(graph.nodes)
+    start_node = nodes[0]
+    for node in nodes:
+        if is_odd(graph.degree[node]):
+            start_node = node
+            break
+    return get_eulerian_tour(graph, start_node, [start_node])
+
+'''takes a graph and returns a 1.5 approximation of the optimal TSP solution using christofides algorithm'''
+def christofides_algorithm(graph):
+    # broken into steps taken from the wikipedia page on the algorithm
+
+    # Create a minimum spanning tree of graph.
+    mst = kruskals_algorithm(graph)
+    # Find the odd degree vertices of the mst
+    odd_degree = find_odd_degree(mst)
+    # Find a minimum-weight perfect matching in the subgraph induced in G by O.
+    mpm = blossom_algorithm(odd_degree)
+    # Combine the edges of the mst and the mpm to form a connected multigraph in which each vertex has even degree.
+    multigraph = nx.compose(mst, mpm)
+    # Form an Eulerian circuit in H.
+    circuit = fleurys_algorithm(multigraph)
+    # Make the circuit found in previous step into a Hamiltonian circuit by skipping repeated vertices (shortcutting).
+    
+    return circuit
+
+G = nx.Graph()
+nx.DiGraph(directed = False)
+edges = [(1,2),(2,3),(3,4),(1,4)]
+G.add_edges_from(edges)
+
+print(fleurys_algorithm(G))
